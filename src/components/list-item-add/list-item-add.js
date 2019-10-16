a21f5e44aaf932e7934da63e86e356793a961f27
Oleksandr <lysenkoa86@gmail.com>
2019-10-16 21:39:49 +0300

    Commit

--- src/components/list-item-add/list-item-add.js
+++ src/components/list-item-add/list-item-add.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './list-item-add.css';
 
-const ListItemAdd = ({ onAdded }) => {
-    return <button
-        type="button"
-        className="btn btn-outline-secondary app-list-item-add"
-        onClick={ () => { onAdded('Some text'); } }>Add item</button>;
-}
+export default class ListItemAdd extends Component {
+    state = {
+        text: ''
+    }
 
-export default ListItemAdd;
+    onTextChange = (e) => {
+        this.setState({
+            text: e.target.value.toUpperCase()
+        });
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        if (this.state.text) {
+            this.props.onAdded(this.state.text);
+            this.setState({
+                text: ''
+            });
+        }
+    }
+
+    render () {
+        return (
+            <form className="d-flex app-list-item-add-form" onSubmit={ this.onSubmit }>
+                <input
+                    type="text"
+                    className="form-control app-list-item-add-input"
+                    onChange={ this.onTextChange }
+                    placeholder="Enter new task..."
+                    value={ this.state.text } />
+                <button className="btn btn-outline-secondary app-list-item-add-btn">Add item</button>
+            </form>
+        );
+    }
+}
