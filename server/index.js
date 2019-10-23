65fbc38804cc850549965780d9908a65f411f135
Oleksandr <lysenkoa86@gmail.com>
2019-10-23 11:31:21 +0300

    Commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost/taskmanager-db', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+
+app.use(bodyParser.json());
+app.use('/', require('./router'));
+
+app.listen(3003, () => {
+    console.log('Server was started...');
+});
